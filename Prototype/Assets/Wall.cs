95cc3f30ff57f225bbb0a7f193044eff7d282d26
jeffmur <jeffmur@uw.edu>
2019-10-25 14:51:24 -0700

    This is how you do it Jayden

---
+++ Prototype/Assets/Wall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+   
+}
