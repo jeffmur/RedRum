030ce7856bc2a540f9605020bb172dc2759fdbf9
adneytran <37353282+adneytran@users.noreply.github.com>
2019-10-28 15:49:44 -0700

    Added UI for health (#4)
    TY

---
+++ Prototype/Assets/Scripts/GameWorld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWorld : MonoBehaviour
+{
+    private GameObject player;
+    private PlayerStats stats;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        stats = player.GetComponent<PlayerStats>();
+        Debug.Assert(player != null);
+        Debug.Assert(stats != null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getStartingHealth()
+    {
+        return stats.MaxHealth;
+    }
+}
---
+++ Prototype/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item
+{
+    protected static int idNumber = 0;
+    protected int itemID;
+    protected bool alreadySpawned = false;
+
+    public abstract void process();
+
+    public void triggerSpawn()
+    {
+        alreadySpawned = true;
+    }
+
+    public bool getAlreadySpawned() { return alreadySpawned; }
+}
