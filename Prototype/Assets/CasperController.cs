66ee3ec5861fc0b5a6520c1a35f6d482f15d27be
christopherdokeefe <cdok@uw.edu>
2019-10-27 22:30:42 -0700

    Casper animation and new tool
    Animated Casper and imported a tool to easily import animations from aseperite

---
+++ Prototype/Assets/CasperController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasperController : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    private Vector2 movement;
+    // Update is called once per frame
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+      
+        animator.SetFloat("HorizontalDirection", movement.x);
+        animator.SetFloat("VerticalDirection", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
