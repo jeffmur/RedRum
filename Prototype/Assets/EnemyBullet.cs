ca26678b95122e8bf011649530923a26cd18cf1b
Chris O'Keefe <cdok@uw.edu>
2019-11-03 21:59:49 -0800

    Added Slime Enemy Prefab

---
+++ Prototype/Assets/EnemyBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public int bulletDamage = 1;
+    public float bulletSpeed = 3f;
+    private Vector3 bulletDirection;
+
+    void Update()
+    {
+        if (bulletDirection != null)
+            transform.localPosition +=  bulletDirection * bulletSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+            DestroyImmediate(gameObject);
+        }
+        else if (other.CompareTag("Player"))
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+            other.GetComponent<PlayerStats>().loseHealth(bulletDamage);
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    public void SetBulletDirection(Vector3 direction)
+    {
+        bulletDirection = direction;
+    }
+}
