a4dc7ea0dca829c14429e97d7eb5954f670b1984
adneytran <37353282+adneytran@users.noreply.github.com>
2019-11-27 04:47:38 -0800

    streamlined TimeManager

---
+++ Prototype/Assets/SlowMotion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SlowMotion
+{
+    public static IEnumerator DoSlowMotion(float length, float slowFactor)
+    {
+        Time.timeScale = slowFactor;
+        Time.fixedDeltaTime = Time.unscaledDeltaTime;
+        while (Time.timeScale < 1f)
+        {
+            Debug.Log(Time.timeScale);
+            // linearly increase the timescale
+            Time.timeScale += (1f / length) * Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+            yield return null;
+        }
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = Time.unscaledDeltaTime;
+    }
+}
