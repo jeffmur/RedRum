5339b08db2188c45ac45b185e490e1ef62a70b38
adneytran <37353282+adneytran@users.noreply.github.com>
2019-12-06 05:24:29 -0800

    added simple blink item

---
+++ Prototype/Assets/Scripts/Items/Active/Blink.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : ActivatedItem
+{
+    protected override void doItemEffect()
+    {
+        Vector3 blinkPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        blinkPosition.z = 0;
+        Casper.Instance.transform.localPosition = blinkPosition;
+    }
+
+    protected override void endItemEffect()
+    {
+        return;
+    }
+
+    protected override void setActivateItemBehavior()
+    {
+        return;
+    }
+
+    protected override void setItemDurations()
+    {
+        cooldownDuration = 0;
+        effectDuration = 0;
+    }
+
+    protected override void setItemInfo()
+    {
+        itemName = "Blink";
+        caption = "It's nothing personal";
+    }
+}
