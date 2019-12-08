8173c43c22a9cdbef0e9c5b04d5d6f2599dd5da5
Chris O'Keefe <cdok@uw.edu>
2019-12-08 03:05:02 -0800

    Laser bitch
    -World space my world ass

--- Prototype/Assets/Scripts/Weapons/SniperRifle.cs
+++ Prototype/Assets/Scripts/Weapons/SniperRifle.cs
@@ -6,15 +6,22 @@ public class SniperRifle : Weapon
 {
     public LineRenderer laser;
     private GameObject crosshair;
+    private Transform weaponInventory;
     
     private void Start()
     {
         crosshair = GameObject.Find("crossHairs");
+        laser.useWorldSpace = true;
+        laser.enabled = false;
     }
     void Shoot()
     {
-        float dist = Vector3.Distance(crosshair.transform.position, transform.position);
-        laser.SetPosition(1, new Vector3(dist, 0, 0));
+        if (transform.parent != null)
+        {
+            laser.enabled = true;
+            laser.SetPosition(0, transform.position);
+            laser.SetPosition(laser.positionCount - 1, crosshair.transform.position);
+        }
     }
 
     // Update is called once per frame
