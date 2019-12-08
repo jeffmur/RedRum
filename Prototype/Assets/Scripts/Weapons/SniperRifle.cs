6bb9c313ed7153088f96d617f5565bc310027b5b
jeffmur <jeffmur@uw.edu>
2019-12-08 02:04:41 -0800

    help chris

---
+++ Prototype/Assets/Scripts/Weapons/SniperRifle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperRifle : Weapon
+{
+    public LineRenderer laser;
+    private GameObject crosshair;
+    
+    private void Start()
+    {
+        crosshair = GameObject.Find("crossHairs");
+    }
+    void Shoot()
+    {
+        float dist = Vector3.Distance(crosshair.transform.position, transform.position);
+        laser.SetPosition(1, new Vector3(dist, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+}
