cddb758b8cf964032e18c34144e5992e171dd180
Clause828 <55769079+Clause828@users.noreply.github.com>
2019-12-08 20:41:22 -0800

    Music for floors

---
+++ Prototype/Assets/Scripts/SoundTracks.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTracks : MonoBehaviour
+{
+    public AudioSource SoundTrack;
+    public AudioClip BossMusic, IdleMusic, WaveMusic, Creepy;
+
+    private void Start()
+    {
+        EventManager.Instance.on
+        EventManager.Instance.onDamaged += DamagedSound;
+        EventManager.Instance.onHealed += HealedSound;
+        EventManager.Instance.onItemPickup += ItemPickupSound;
+        EventManager.Instance.onWeaponAdded += WeaponPickupSound;
+        EventManager.Instance.onWeaponReloaded += WeaponPickupSound;
+
+        StartCoroutine(InitSound());
+    }
+
+    private IEnumerator InitSound()
+    {
+        SoundTrack.volume = 0;
+        yield return new WaitForSeconds(1);
+        SoundTrack.volume = 1;
+    }
+
+    private void IdleSound()
+    {
+        SoundTrack.clip = IdleMusic;
+        SoundTrack.Play();
+    }
+
+    private void Level2Sound()
+    {
+        SoundTrack.clip = WaveMusic;
+        SoundTrack.Play();
+    }
+
+    private void BossSound()
+    {
+        SoundTrack.clip = BossMusic;
+        SoundTrack.Play();
+    }
+
+    private void Level1Sound()
+    {
+        SoundTrack.clip = Creepy;
+        SoundTrack.Play();
+    }
+}
