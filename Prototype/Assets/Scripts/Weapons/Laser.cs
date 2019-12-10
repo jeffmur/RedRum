0fcd6f071701d03bea92bae7d5516758c9744e24
Clause828 <55769079+Clause828@users.noreply.github.com>
2019-12-10 03:33:22 -0800

    Actuall Sniper bulletPrefab for bullet

--- Prototype/Assets/Scripts/Weapons/Laser.cs
+++ Prototype/Assets/Scripts/Weapons/Laser.cs
@@ -6,20 +6,21 @@ public class Laser : MonoBehaviour
 {
     public LineRenderer laser;
     private GameObject crosshair;
+    private Weapon sniper;
     private bool waitToShow = false;
     private void Start()
     {
         crosshair = GameObject.Find("crossHairs");
+        sniper = gameObject.GetComponentInParent<Weapon>(); // if we wanted to map properly to the sniper offset
     }
     void Shoot()
     {
-        laser.useWorldSpace = true;
+        laser.useWorldSpace = true; //wait for the Sniper to be picked up 
         laser.enabled = waitToShow;
         if (transform.parent != null)
         {
             waitToShow = true;
             laser.enabled = waitToShow;
-            // THIS IS FUCKING STUPID
             laser.SetPosition(0, transform.position);
             laser.SetPosition(laser.positionCount - 1, crosshair.transform.position);
         }
